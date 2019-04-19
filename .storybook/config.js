0fb1dbc765dd6a18e43ee097ef08204aa272a3bc
João Belo <jpasbelo@gmail.com>
2019-04-19 18:31:50 +0200

    adds theming to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,15 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react'
+import {withThemesProvider} from 'storybook-addon-styled-component-theme'
 
-const req = require.context('../src/components', true, /\.stories\.js$/);
+import theme from '../src/theme'
+
+const req = require.context('../src/components', true, /\.stories\.js$/)
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
+
+const themes = [theme]
+addDecorator(withThemesProvider(themes))
